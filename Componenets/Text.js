92d526ceddea6265c23f95915427aad007a20477
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2024-09-14 22:03:55 +0530

    All files are component files in my Reactjs project

---
+++ Componenets/Text.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Text(props) {
+  return (
+  
+<div classname="mb-3">
+    <h1>{props.Haeding}</h1>
+
+  <div class="form-group">
+    <label for="My Box"></label>
+    <input class="form-control input-lg" id="My Box" type="text"/>
+  </div>
+  
+  </div>
+    
+  )
+}
